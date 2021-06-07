a2bbb6b0724b742a3068bd4b634a11cc6a8d0ffe
mahesh1926 <83626908+mahesh1926@users.noreply.github.com>
2021-06-07 10:36:48 +0530

    Add files via upload

---
+++ addTwoArrays.java
@@ -0,0 +1,14 @@
+package aPgm;
+
+public class addTwoArrays {
+
+	public static void main(String[] args) {
+		int a[]= {2,3,4,5};
+		int b[]= {4,5,3,4};
+		for (int i = 0; i < b.length; i++) {
+			System.out.println(a[i]+b[i]);
+		}
+
+	}
+
+}
